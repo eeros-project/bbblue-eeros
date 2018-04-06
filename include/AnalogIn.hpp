671defd6e3b15b49acb10646351459c2758cca34
Andreas Kunz <andreas.kunz@ntb.ch>
2018-04-06 17:26:47 +0200

    feat(adc_to_AnalogIn) created AnalogIn class. added to CMakeLists.

---
+++ include/AnalogIn.hpp
@@ -0,0 +1,29 @@
+#ifndef BBBLUE_EEROS_ANALOGIN_HPP_
+#define BBBLUE_EEROS_ANALOGIN_HPP_
+
+#include <eeros/hal/ScalableInput.hpp>
+#include <limits>
+#include "BBBlueDevice.hpp"
+
+
+namespace bbblue {
+	class AnalogIn : public eeros::hal::ScalableInput<double> {
+	public:
+		AnalogIn(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
+		    double scale = 1, double offset = 0, 
+		    double rangeMin = std::numeric_limits<double>::min(), double rangeMax = std::numeric_limits<double>::max(), 
+		    std::string unit = "", bool getDelta = false);
+		virtual double get();
+		
+	private:
+		int32_t channel;
+	};
+};
+
+extern "C"{
+	eeros::hal::ScalableInput<double> *createAnalogIn(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, 
+						     uint32_t channel, double scale, double offset, 
+						     double rangeMin, double rangeMax, std::string unit);
+}
+
+#endif /* BBBLUE_EEROS_ANALOGIN_HPP_ */
