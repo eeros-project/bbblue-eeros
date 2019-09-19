2d0268a04e3c4df49c087f609da2e8aade722a60
ursgraf <urs.graf@ntb.ch>
2019-09-19 17:07:32 +0200

    Allow reading back of analog output value

--- lib/AnalogOut.cpp
+++ lib/AnalogOut.cpp
@@ -10,27 +10,25 @@ using namespace bbblue;
 using namespace eeros::hal;
 
 AnalogOut::AnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
-		     double scale, double offset, double rangeMin, double rangeMax, std::string unit) : 
-		     ScalableOutput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit), channel(channel) {
+                     double scale, double offset, double rangeMin, double rangeMax, std::string unit) 
+    : ScalableOutput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit), channel(channel) {
 	rc_enable_motors();
 }
 
-double AnalogOut::get() { return 0; }
-
-void AnalogOut::setValue(uint32_t value) { }
+double AnalogOut::get() {return value * scale + offset;}
 
 void AnalogOut::set(double voltage) {
-	double value = (voltage - offset) / scale;
-	if( value > maxOut ) value = maxOut;
-	if( value < minOut ) value = minOut;
-	rc_set_motor(channel, (float)value);	
+  value = (voltage - offset) / scale;
+  if (value > maxOut) value = maxOut;
+  if (value < minOut) value = minOut;
+  rc_set_motor(channel, (float)value);	
 }
 
 extern "C"{
-	eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void *libHandle, std::string device, uint32_t subDeviceNumber, 
-							    uint32_t channel, double scale, double offset, double rangeMin, double rangeMax, std::string unit){
-		return new bbblue::AnalogOut(id, libHandle, device, subDeviceNumber, channel, scale, offset, rangeMin, rangeMax, unit);
-	}
+  eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void *libHandle, std::string device, uint32_t subDeviceNumber, 
+                                                      uint32_t channel, double scale, double offset, double rangeMin, double rangeMax, std::string unit) {
+    return new bbblue::AnalogOut(id, libHandle, device, subDeviceNumber, channel, scale, offset, rangeMin, rangeMax, unit);
+  }
 }
 
 
