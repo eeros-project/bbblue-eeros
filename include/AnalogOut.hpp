b7e12d07b11ee34255742e662d2a3b6d51129c58
ursgraf <urs.graf@ost.ch>
2021-05-15 12:16:39 +0200

    Add servo channels

--- include/AnalogOut.hpp
+++ include/AnalogOut.hpp
@@ -3,6 +3,7 @@
 
 #include <eeros/hal/ScalableOutput.hpp>
 #include <limits>
+#include <chrono>
 
 namespace bbblue {
   
@@ -13,10 +14,15 @@ class AnalogOut : public eeros::hal::ScalableOutput<double> {
   virtual ~AnalogOut();
   virtual double get();
   virtual void set(double value);
+  void freeSpin(int ch);
+  void brake(int ch);
 
  private:
   uint32_t channel;
   double value;
+  bool measure = true;
+  uint32_t count = 0, maxCount = 1000;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
 };
 
 }
@@ -24,6 +30,10 @@ class AnalogOut : public eeros::hal::ScalableOutput<double> {
 extern "C"{
   eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
                                                       double scale, double offset, double rangeMin, double rangeMax, std::string unit);
+void freeSpin(bbblue::AnalogOut *obj, int ch);
+
+void brake(bbblue::AnalogOut *obj, int ch);
+  
 }
 
 #endif /* BBBLUE_EEROS_ANALOGOUT_HPP_ */
--- lib/AnalogOut.cpp
+++ lib/AnalogOut.cpp
@@ -1,4 +1,5 @@
 #include "../include/AnalogOut.hpp"
+#include <chrono>
 
 extern "C" {
 #include <robotcontrol.h>
@@ -10,27 +11,64 @@ using namespace eeros::hal;
 AnalogOut::AnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
                      double scale, double offset, double rangeMin, double rangeMax, std::string unit) 
     : ScalableOutput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit), channel(channel) {
-  rc_motor_init();
+  if (channel < 10) rc_motor_init();
+  else {
+    rc_servo_init();
+    rc_servo_power_rail_en(1);
+  }
 }
 
 AnalogOut::~AnalogOut() {
   rc_motor_cleanup();
+  rc_servo_cleanup();
 }
 
-
-double AnalogOut::get() {return value * scale + offset;}
+double AnalogOut::get() {
+  return value * scale + offset;
+}
 
 void AnalogOut::set(double voltage) {
   value = (voltage - offset) / scale;
   if (value > maxOut) value = maxOut;
   if (value < minOut) value = minOut;
-  rc_motor_set(channel, (float)value);	
+  if (channel < 10) rc_motor_set(channel, (float)value);
+  else {  // make sure that update of servo signal is not faster than 50Hz
+    if (measure) {
+      if (count == 0) start = std::chrono::high_resolution_clock::now();
+      else if (count == 1) {
+        double diff = std::chrono::duration<double, std::micro>(std::chrono::high_resolution_clock::now()-start).count();
+        maxCount = 1000 * 40 / diff;  // 40ms update period -> 25Hz
+        measure = false;
+      }
+    }
+    count++;
+    if (count >= maxCount) {
+      rc_servo_send_pulse_normalized(channel - 10, value);
+      count = 0;
+    }
+  }
+}
+
+void AnalogOut::freeSpin(int ch) {
+  rc_motor_free_spin(ch);
+}
+
+void AnalogOut::brake(int ch) {
+  rc_motor_brake(ch);
 }
 
 extern "C"{
   eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void *libHandle, std::string device, uint32_t subDeviceNumber, 
                                                       uint32_t channel, double scale, double offset, double rangeMin, double rangeMax, std::string unit) {
     return new bbblue::AnalogOut(id, libHandle, device, subDeviceNumber, channel, scale, offset, rangeMin, rangeMax, unit);
+  }
+
+  void freeSpin(bbblue::AnalogOut *obj, int ch) {
+    obj->freeSpin(ch);
+  }
+  
+  void brake(bbblue::AnalogOut *obj, int ch) {
+    obj->brake(ch);
   }
 }
 
