8cf596b3b935caaabe2ee086b0f59b42b6b27226
ursgraf <urs.graf@ost.ch>
2021-06-10 13:47:35 +0200

    Improve parallel reading of mpu and bmp

--- include/AnalogIn.hpp
+++ include/AnalogIn.hpp
@@ -15,7 +15,7 @@ class AnalogIn : public eeros::hal::ScalableInput<double> {
  public:
   AnalogIn(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
            double scale = 1, double offset = 0, double rangeMin = std::numeric_limits<double>::min(), 
-           double rangeMax = std::numeric_limits<double>::max(), std::string unit = "", bool getDelta = false);
+           double rangeMax = std::numeric_limits<double>::max(), std::string unit = "");
   virtual ~AnalogIn();
   virtual double get();
 
@@ -23,9 +23,12 @@ class AnalogIn : public eeros::hal::ScalableInput<double> {
   int32_t channel;
   static rc_mpu_config_t mpuConfig;
   static rc_mpu_data_t mpuData;
-  rc_bmp_data_t barData;
-//   static uint32_t accState, gyroState, magState;
+  static rc_bmp_data_t bmpData;
   static bool first;
+  bool measure = true;
+  uint32_t count = 0, maxCount = 1000;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
+  static void dmpHandler(void);
 };
 
 }
--- lib/AnalogIn.cpp
+++ lib/AnalogIn.cpp
@@ -10,25 +10,30 @@ using namespace bbblue;
 using namespace eeros::hal;
 
 AnalogIn::AnalogIn(std::string id,  void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, double scale,
-                   double offset, double rangeMin, double rangeMax, std::string unit, bool getDelta) 
+                   double offset, double rangeMin, double rangeMax, std::string unit) 
     : ScalableInput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit), channel(channel) { 
   rc_adc_init();
-  if (channel >= 30 && channel < 42 && first) {
+  if (channel >= 30 && channel <= 42 && first) {
+    // reading the imu and barometer over the same i2c is tricky, make sure that 
+    // bmp is initialized even if it is not used, imu read must be finished when 
+    // accessing the barometer 
+    while (rc_bmp_init(BMP_OVERSAMPLE_16, BMP_FILTER_16)) std::cout << "init bmp again" << std::endl;
     mpuConfig = rc_mpu_default_config();
     mpuConfig.enable_magnetometer = 1;
     mpuConfig.dmp_fetch_accel_gyro = 1;
-    mpuConfig.dmp_sample_rate = 100;
+    mpuConfig.dmp_sample_rate = 200;
+    mpuConfig.show_warnings = 0;
     rc_mpu_initialize_dmp(&mpuData, mpuConfig);
-//     rc_mpu_set_dmp_callback(&callback);
     first = false;
-    usleep(100000); // wait for initialization
+    usleep(100000); // wait for dmp to settle then start callback
+    rc_mpu_set_dmp_callback(AnalogIn::dmpHandler);
   }
-  if (channel == 43) rc_bmp_init(BMP_OVERSAMPLE_4, BMP_FILTER_OFF);
 }
 
 AnalogIn::~AnalogIn() {
   rc_adc_cleanup();
   rc_mpu_power_off();
+  rc_bmp_power_off();
 }
 
 /**
@@ -41,42 +46,23 @@ double AnalogIn::get() {
   else if (channel == 20) val = rc_adc_dc_jack();
   else if (channel == 21) val = rc_adc_batt();
   else if (channel >= 30 && channel <= 32) { // read the mpu just once for all channels 
-//     if (accState & (1 << (channel - 30))) {
-//       accState = 1 << (channel - 30);
-// //       rc_mpu_read_accel(&mpuData);
-//     } else accState |= 1 << (channel - 30);
     if (channel == 30) val = mpuData.raw_accel[0];
     else if (channel == 31) val = mpuData.raw_accel[1]; 
     else if (channel == 32) val = mpuData.raw_accel[2];
   } else if (channel >= 33 && channel <= 35) { // read the mpu just once for all channels 
-//     if (gyroState & (1 << (channel - 30))) {
-//       gyroState = 1 << (channel - 30);
-// //       rc_mpu_read_gyro(&mpuData);
-//     } else gyroState |= 1 << (channel - 30);
     if (channel == 33) val = mpuData.raw_gyro[0];
     else if (channel == 34) val = mpuData.raw_gyro[1];
     else if (channel == 35) val = mpuData.raw_gyro[2];
   } else if (channel >= 36 && channel <= 38) { // read the mpu just once for all channels 
-//     if (magState & (1 << (channel - 30))) {
-//       magState = 1 << (channel - 30);
-// //       rc_mpu_read_mag(&mpuData);
-//     } else magState |= 1 << (channel - 30);
     if (channel == 36) val = mpuData.mag[0];
     else if (channel == 37) val = mpuData.mag[1];
     else if (channel == 38) val = mpuData.mag[2];
   } else if (channel >= 39 && channel <= 42) { // read the mpu just once for all channels 
-//     if (magState & (1 << (channel - 30))) {
-//       magState = 1 << (channel - 30);
-// //       rc_mpu_read_mag(&mpuData);
-//     } else magState |= 1 << (channel - 30);
     if (channel == 39) val = mpuData.dmp_quat[0];
     else if (channel == 40) val = mpuData.dmp_quat[1];
     else if (channel == 41) val = mpuData.dmp_quat[2];
     else if (channel == 42) val = mpuData.dmp_quat[3];
-  } else if (channel == 43) { 
-    rc_bmp_read(&barData);
-    val = barData.alt_m;
-  }
+  } else if (channel == 43) val = bmpData.alt_m;
 
   double value = (val - offset) / scale;
   if( value > maxIn ) value = maxIn;
@@ -84,11 +70,18 @@ double AnalogIn::get() {
   return value;
 }
 
+void AnalogIn::dmpHandler(void) {
+  static int count = 0;
+  count++;
+  if (count >= 10) { // check if we need to sample BMP 
+    if(rc_bmp_read(&bmpData)) return;
+    count = 0;
+  }
+}
+
 rc_mpu_data_t AnalogIn::mpuData;
 rc_mpu_config_t AnalogIn::mpuConfig;
-// uint32_t AnalogIn::accState = 0;
-// uint32_t AnalogIn::gyroState = 0;
-// uint32_t AnalogIn::magState = 0;
+rc_bmp_data_t AnalogIn::bmpData;
 bool AnalogIn::first = true;
 
 extern "C"{
